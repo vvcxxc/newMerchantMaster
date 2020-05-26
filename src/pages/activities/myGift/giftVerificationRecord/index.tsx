ed2dc93a7690c72ca5a7634184c5f73d9ca8cbc9
vvcxxc <asd123>
2020-05-26 11:06:14 +0800

    misc：文件夹

---
+++ src/pages/activities/myGift/giftVerificationRecord/index.tsx
@@ -0,0 +1,20 @@
+/**title: 我的活动 */
+import React, { Component } from 'react';
+import { Flex, WingBlank, Button, InputItem, Toast, View } from 'antd-mobile';
+import styles from './index.less';
+import request from '@/services/request'
+import router from 'umi/router';
+export default class Notice extends Component<any> {
+    state = {
+
+    };
+
+    render() {
+
+        return (
+            <View style={{ position: 'fixed', height: '100vh', width: '100vw', overflow: 'scroll', top: 0, left: 0 }}>
+
+            </View>
+        )
+    }
+}
