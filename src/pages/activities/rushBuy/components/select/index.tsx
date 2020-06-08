66f8d1e66a004366b4c913a8226a5b1abfba7133
songyizhe <769724661@qq.com>
2020-06-08 15:10:29 +0800

    feat: 抢购活动列表

--- src/pages/activities/rushBuy/components/select/index.tsx
+++ src/pages/activities/rushBuy/components/select/index.tsx
@@ -6,13 +6,23 @@ export default function Select() {
   const [order_sort, setOrder] = useState(0); // 0为默认，1为升序，2为降序
   const [verification_sort, setVerification] = useState(0); // 0为默认，1为升序，2为降序
   const [sell_sort, setSell] = useState(0); // 0为默认，1为升序，2为降序
-  const [type, setType] = useState(2); // 1为即将开始，2为抢购中，3为已结束
+  const [type, setType] = useState(1); // 1为即将开始，2为抢购中，3为已结束
   const [is_show, setShow] = useState(false); // 控制筛选条件展示
   const [show_sort, setShowSort] = useState(true); // 控制展示排序，为即将开始不展示
+  const [coupon_type, setCouponType] = useState(1); // 抢购券类型，1为现金，2为兑换
+
+  useEffect(()=>{
+    if (type === 1) {
+      setShowSort(false)
+    } else {
+      setShowSort(true)
+    }
+  },[])
 
   useEffect(() => {
     console.log(order_sort, verification_sort)
-  }, [order_sort, verification_sort, sell_sort, type])
+
+  }, [order_sort, verification_sort, sell_sort, type, coupon_type])
 
   const onSort = (type: string) => {
     if (type === 'order') {
@@ -59,63 +69,73 @@ export default function Select() {
     setShow(false)
   }
 
+  const CouponType = (type: number) => {
+    setCouponType(type)
+  }
+
   return (
-    <div className={styles.select_box}>
-      <Flex align='center' justify='center' className={styles.bottomRight} style={{ color: '#4486F7' }} onClick={() => { setShow(true) }}>
-        <div className={styles.title}>{type === 1 ? '即将开始' : type === 2 ? '抢购中' : type === 3 ? '已结束' : null}</div>
-        <div>
-          <div className={styles.triangle_down}></div>
-        </div>
+    <div className={styles.selectBox}>
+      <Flex className={styles.type_box}>
+        <div className={coupon_type === 2 ? styles.type_item_activated : styles.type_item} onClick={CouponType.bind(this, 2)}>商品券</div>
+        <div className={coupon_type === 1 ? styles.type_item_activated : styles.type_item} onClick={CouponType.bind(this, 1)}>现金券</div>
+      </Flex>
+      <Flex className={styles.select_box}>
+        <Flex align='center' justify='center' className={styles.bottomRight} style={{ color: '#4486F7' }} onClick={() => { setShow(true) }}>
+          <div className={styles.title}>{type === 1 ? '即将开始' : type === 2 ? '抢购中' : type === 3 ? '已结束' : null}</div>
+          <div>
+            <div className={styles.triangle_down}></div>
+          </div>
+        </Flex>
+        {
+          show_sort ? <Flex align='center' justify='center' onClick={onSort.bind(this, 'order')} className={styles.bottomRight}>
+            <div className={styles.title}>订单总数</div>
+            <div>
+              <div className={order_sort === 1 ? styles.triangle_up : styles.triangle_up_no}></div>
+              <div className={order_sort === 2 ? styles.triangle_down : styles.triangle_down_no}></div>
+            </div>
+          </Flex> : null
+        }
+        {
+          show_sort ? <Flex align='center' justify='center' onClick={onSort.bind(this, 'verification')} className={styles.bottomRight}>
+            <div className={styles.title}>核销量</div>
+            <div>
+              <div className={verification_sort === 1 ? styles.triangle_up : styles.triangle_up_no}></div>
+              <div className={verification_sort === 2 ? styles.triangle_down : styles.triangle_down_no}></div>
+            </div>
+          </Flex> : null
+        }
+        {
+          show_sort ? <Flex align='center' justify='center' style={{ width: '25%' }} onClick={onSort.bind(this, 'sell')}>
+            <div className={styles.title}>销售额</div>
+            <div>
+              <div className={sell_sort === 1 ? styles.triangle_up : styles.triangle_up_no}></div>
+              <div className={sell_sort === 2 ? styles.triangle_down : styles.triangle_down_no}></div>
+            </div>
+          </Flex> : null
+        }
+
+
+
+
       </Flex>
       {
-        show_sort ? <Flex align='center' justify='center' onClick={onSort.bind(this, 'order')} className={styles.bottomRight}>
-          <div className={styles.title}>订单总数</div>
-          <div>
-            <div className={order_sort === 1 ? styles.triangle_up : styles.triangle_up_no}></div>
-            <div className={order_sort === 2 ? styles.triangle_down : styles.triangle_down_no}></div>
-          </div>
-        </Flex> : null
-      }
-      {
-        show_sort ? <Flex align='center' justify='center' onClick={onSort.bind(this, 'verification')} className={styles.bottomRight}>
-          <div className={styles.title}>核销量</div>
-          <div>
-            <div className={verification_sort === 1 ? styles.triangle_up : styles.triangle_up_no}></div>
-            <div className={verification_sort === 2 ? styles.triangle_down : styles.triangle_down_no}></div>
-          </div>
-        </Flex> : null
-      }
-      {
-        show_sort ? <Flex align='center' justify='center' style={{width: '25%'}} onClick={onSort.bind(this, 'sell')}>
-          <div className={styles.title}>销售额</div>
-          <div>
-            <div className={sell_sort === 1 ? styles.triangle_up : styles.triangle_up_no}></div>
-            <div className={sell_sort === 2 ? styles.triangle_down : styles.triangle_down_no}></div>
-          </div>
-        </Flex> : null
-      }
-
-
-
-      {
-        is_show ? <div className={styles.mark}>
-          <div className={styles.choose_box}>
-            <Flex onClick={handleAction.bind(this, 1)} justify='between' className={type === 1 ? styles.active : styles.choose_item}>
-              <div>即将开始</div>
-              {type === 1 ? <Icon color='#4486F7' size='sm' type='check' /> : null}
-            </Flex>
-            <Flex onClick={handleAction.bind(this, 2)} justify='between' className={type === 2 ? styles.active : styles.choose_item}>
-              <div>抢购中</div>
-              {type === 2 ? <Icon color='#4486F7' size='sm' type='check' /> : null}
-            </Flex>
-            <Flex onClick={handleAction.bind(this, 3)} justify='between' className={type === 3 ? styles.active : styles.choose_item} style={{ borderBottom: 'none' }}>
-              <div>已结束</div>
-              {type === 3 ? <Icon color='#4486F7' size='sm' type='check' /> : null}
-            </Flex>
-          </div>
-        </div> : null
-      }
-
+          is_show ? <div className={styles.mark}>
+            <div className={styles.choose_box}>
+              <Flex onClick={handleAction.bind(this, 1)} justify='between' className={type === 1 ? styles.active : styles.choose_item}>
+                <div>即将开始</div>
+                {type === 1 ? <Icon color='#4486F7' size='sm' type='check' /> : null}
+              </Flex>
+              <Flex onClick={handleAction.bind(this, 2)} justify='between' className={type === 2 ? styles.active : styles.choose_item}>
+                <div>抢购中</div>
+                {type === 2 ? <Icon color='#4486F7' size='sm' type='check' /> : null}
+              </Flex>
+              <Flex onClick={handleAction.bind(this, 3)} justify='between' className={type === 3 ? styles.active : styles.choose_item} style={{ borderBottom: 'none' }}>
+                <div>已结束</div>
+                {type === 3 ? <Icon color='#4486F7' size='sm' type='check' /> : null}
+              </Flex>
+            </div>
+          </div> : null
+        }
     </div>
   )
 }
--- src/pages/activities/rushBuy/coupon-list/index.tsx
+++ src/pages/activities/rushBuy/coupon-list/index.tsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Select from '../components/select'
 import ListItem from '../components/list-item'
+import Header from '@/components/header'
 import styles from './index.less'
+import { Flex } from 'antd-mobile'
 
 export default class CouponList extends Component {
   state = {
-
+    is_add: false
   }
 
   // 加载更多
@@ -13,17 +15,39 @@ export default class CouponList extends Component {
 
   }
 
-
+  // 添加优惠券
+  addCoupon = () => {
+    console.log(444)
+    this.setState({is_add: true})
+  }
 
 
 
   render (){
+    const { is_add } = this.state
+    const add = (
+      <Flex className={styles.header_icon} direction='column' onClick={this.addCoupon}>
+        <div className={styles.icon_box}></div>
+        <div>添加</div>
+      </Flex>
+    )
     return (
       <div className={styles.list_page}>
+        <Header title='抢购活动' color='dark' style={{background: '#fff'}} rightRender={()=> add} />
         <Select />
         <div className={styles.list}>
           <ListItem />
         </div>
+        {
+          is_add ? <Flex className={styles.add_mark} align='center' justify='center'>
+          <Flex className={styles.add_box} direction='column' align='center'>
+            <div className={styles.add_title}>请选择商品类型</div>
+            <Flex className={styles.add_item_activated} align='center' justify='center'>商品券</Flex>
+            <Flex className={styles.add_item} align='center' justify='center'>现金券</Flex>
+          </Flex>
+        </Flex> : null
+        }
+
       </div>
     )
   }
