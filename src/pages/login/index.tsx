e05a202509162b9c5ab90fd8df1511388a2eee47
vvcxxc <asd123>
2020-06-24 10:19:32 +0800

    styl：样式

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -172,7 +172,7 @@ export default class Login extends Component {
                       </Flex>
                     })
                   }
-                  <input value={code} onChange={this.codeChange} ref={this.input} />
+                  <input  className={styles.code_box_input}  value={code} onChange={this.codeChange} ref={this.input} />
                 </Flex>
                 {
                   time === 0 ? <div className={styles.send_code}>重新发送</div> : <div className={styles.send_code} style={{ color: '#98A6AD' }}>{time}秒后重新发送</div>
